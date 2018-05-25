1faf4d3a4bbebc9f39040498b9d617b005cae3b0
andi-7 <andreas.starzer@gmx.at>
2018-05-25 10:36:05 +0200

    1st upload

---
+++ HelloWorld/HelloWorld/hello.c
@@ -0,0 +1,14 @@
+//
+//  hello.c
+//  Hello World
+//
+//  Created by Andreas Starzer on 25.05.18.
+//  Copyright © 2018 Andreas Starzer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
