b04d9e93ecae528374d6e92e9007c4ebbe5c0d1c
andi-7 <andreas.starzer@gmx.at>
2018-09-25 13:27:32 +0200

    New Folder

---
+++ CPP/Hello World/Hello World/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Andreas Starzer on 14.09.18.
+//  Copyright © 2018 Andreas Starzer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
